b5ad26c8a8c106b15bf3583d4f6ab23b816a5e7a
Shawn <shawnaoates@gmail.com>
2026-08-17 14:33:18 -0400

    update the

--- .source/server.ts
+++ .source/server.ts
@@ -9,11 +9,11 @@ import * as __fd_glob_18 from "../content/help/hosting/creating-a-plan.mdx?colle
 import * as __fd_glob_17 from "../content/help/getting-started/understanding-the-dashboard.mdx?collection=help"
 import * as __fd_glob_16 from "../content/help/getting-started/inviting-people.mdx?collection=help"
 import * as __fd_glob_15 from "../content/help/getting-started/creating-a-calendar.mdx?collection=help"
-import * as __fd_glob_14 from "../content/help/calendars-and-rsvps/notifications.mdx?collection=help"
-import * as __fd_glob_13 from "../content/help/calendars-and-rsvps/how-rsvp-works.mdx?collection=help"
-import * as __fd_glob_12 from "../content/help/calendars-and-rsvps/calendar-sync.mdx?collection=help"
-import * as __fd_glob_11 from "../content/help/billing/subscriptions-and-plans.mdx?collection=help"
-import * as __fd_glob_10 from "../content/help/billing/payment-methods.mdx?collection=help"
+import * as __fd_glob_14 from "../content/help/billing/subscriptions-and-plans.mdx?collection=help"
+import * as __fd_glob_13 from "../content/help/billing/payment-methods.mdx?collection=help"
+import * as __fd_glob_12 from "../content/help/calendars-and-rsvps/notifications.mdx?collection=help"
+import * as __fd_glob_11 from "../content/help/calendars-and-rsvps/how-rsvp-works.mdx?collection=help"
+import * as __fd_glob_10 from "../content/help/calendars-and-rsvps/calendar-sync.mdx?collection=help"
 import * as __fd_glob_9 from "../content/help/account/notification-settings.mdx?collection=help"
 import * as __fd_glob_8 from "../content/help/account/deleting-your-account.mdx?collection=help"
 import * as __fd_glob_7 from "../content/help/index.mdx?collection=help"
@@ -32,4 +32,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>();
 
-export const help = await create.docs("help", "content/help", {"account/meta.json": __fd_glob_0, "billing/meta.json": __fd_glob_1, "calendars-and-rsvps/meta.json": __fd_glob_2, "getting-started/meta.json": __fd_glob_3, "hosting/meta.json": __fd_glob_4, "organizations/meta.json": __fd_glob_5, }, {"faq.mdx": __fd_glob_6, "index.mdx": __fd_glob_7, "account/deleting-your-account.mdx": __fd_glob_8, "account/notification-settings.mdx": __fd_glob_9, "billing/payment-methods.mdx": __fd_glob_10, "billing/subscriptions-and-plans.mdx": __fd_glob_11, "calendars-and-rsvps/calendar-sync.mdx": __fd_glob_12, "calendars-and-rsvps/how-rsvp-works.mdx": __fd_glob_13, "calendars-and-rsvps/notifications.mdx": __fd_glob_14, "getting-started/creating-a-calendar.mdx": __fd_glob_15, "getting-started/inviting-people.mdx": __fd_glob_16, "getting-started/understanding-the-dashboard.mdx": __fd_glob_17, "hosting/creating-a-plan.mdx": __fd_glob_18, "hosting/editing-or-canceling-a-plan.mdx": __fd_glob_19, "hosting/managing-rsvps.mdx": __fd_glob_20, "hosting/plan-chat.mdx": __fd_glob_21, "organizations/adding-and-removing-members.mdx": __fd_glob_22, "organizations/roles-and-permissions.mdx": __fd_glob_23, "organizations/what-is-an-organization.mdx": __fd_glob_24, });
+export const help = await create.docs("help", "content/help", {"account/meta.json": __fd_glob_0, "billing/meta.json": __fd_glob_1, "calendars-and-rsvps/meta.json": __fd_glob_2, "getting-started/meta.json": __fd_glob_3, "hosting/meta.json": __fd_glob_4, "organizations/meta.json": __fd_glob_5, }, {"faq.mdx": __fd_glob_6, "index.mdx": __fd_glob_7, "account/deleting-your-account.mdx": __fd_glob_8, "account/notification-settings.mdx": __fd_glob_9, "calendars-and-rsvps/calendar-sync.mdx": __fd_glob_10, "calendars-and-rsvps/how-rsvp-works.mdx": __fd_glob_11, "calendars-and-rsvps/notifications.mdx": __fd_glob_12, "billing/payment-methods.mdx": __fd_glob_13, "billing/subscriptions-and-plans.mdx": __fd_glob_14, "getting-started/creating-a-calendar.mdx": __fd_glob_15, "getting-started/inviting-people.mdx": __fd_glob_16, "getting-started/understanding-the-dashboard.mdx": __fd_glob_17, "hosting/creating-a-plan.mdx": __fd_glob_18, "hosting/editing-or-canceling-a-plan.mdx": __fd_glob_19, "hosting/managing-rsvps.mdx": __fd_glob_20, "hosting/plan-chat.mdx": __fd_glob_21, "organizations/adding-and-removing-members.mdx": __fd_glob_22, "organizations/roles-and-permissions.mdx": __fd_glob_23, "organizations/what-is-an-organization.mdx": __fd_glob_24, });
